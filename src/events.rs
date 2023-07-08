811781b9bfa600956e6ced05de8bfb73fe2f8fa9
truongchanbuu <truongchanbuu0512@gmail.com>
2023-07-08 14:09:30 +0700

    Homework

--- src/events.rs
+++ src/events.rs
@@ -1,8 +1,8 @@
 use std::fmt;
 
 use near_sdk::{
-  serde::{Deserialize, Serialize},
-  Balance,
+    serde::{Deserialize, Serialize},
+    Balance,
 };
 
 use crate::Product;
@@ -11,30 +11,33 @@ use crate::Product;
 #[serde(crate = "near_sdk::serde")]
 #[non_exhaustive]
 pub enum EventLogVariant {
-  Purchase(Vec<PurchaseProduct>),
+    Purchase(Vec<PurchaseProduct>),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "near_sdk::serde")]
 pub struct PurchaseProduct {
-  pub owner_id: String,
-  pub product_info: String,
-  pub price: Balance,
-  #[serde(skip_serializing_if = "Option::is_none")]
-  pub memo: Option<String>,
+    pub owner_id: String,
+    pub product_info: String,
+    pub price: Balance,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub memo: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "near_sdk::serde")]
 pub struct EventLog {
-  pub standard: String,
+    pub standard: String,
 
-  #[serde(flatten)]
-  pub event: EventLogVariant,
+    #[serde(flatten)]
+    pub event: EventLogVariant,
 }
 
 impl fmt::Display for EventLog {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    f.write_fmt(format_args!("EVENT_JSON:{}", &serde_json::to_string(self).map_err(|_| fmt::Error)?))
-  }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!(
+            "EVENT_JSON:{}",
+            &serde_json::to_string(self).map_err(|_| fmt::Error)?
+        ))
+    }
 }
